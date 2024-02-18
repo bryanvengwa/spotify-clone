d80c9ec61d34c0e0563c807bdcfd92a82096aa31
bryanvengwa <bmvengwa@gmail.com>
2024-02-18 10:33:03 +0200

    added the configure module to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
@@ -8,4 +8,8 @@ import { SongsModule } from './songs/songs.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    
+  }
+}
