8d3b951704c73b88786a8389b7f26f6e64dcb2a1
bryanvengwa <bmvengwa@gmail.com>
2024-04-12 13:19:33 +0200

    implemented CR opeations using the song service

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Delete, Get, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { SongsService } from './songs.service';
 
 @Controller('songs')
 export class SongsController {
+  constructor(private songService: SongsService) {}
+
+  @Post()
+  create() {
+    this.songService.create('First person shooter');
+    return this.songService.findAll();
+  }
+
   @Get()
   findAllSongs() {
-    return 'find all songs endpoint';
+    return this.songService.findAll();
   }
 
   @Get(':id')
@@ -16,7 +25,5 @@ export class SongsController {
     return 'update song based on the id';
   }
   @Delete(':id')
-  delete(){
-
-  }
+  delete() {}
 }
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SongsService {}
+export class SongsService {
+  // local array
+
+  private readonly songs = [];
+  create(song) {
+    this.songs.push(song);
+    return this.songs;
+  }
+  findAll() {
+    return this.songs;
+  }
+}
