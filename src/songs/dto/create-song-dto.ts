9c07a70958e5e1cfc279ff2e2cf7d9fcf5053477
bryanvengwa <bmvengwa@gmail.com>
2024-04-13 10:19:51 +0200

    working post songs route

--- src/songs/dto/create-song-dto.ts
+++ src/songs/dto/create-song-dto.ts
@@ -1,12 +1,17 @@
-import { IsString, IsNotEmpty, IsArray, IsDateString, IsMilitaryTime } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsArray,
+  IsDateString,
+  IsMilitaryTime,
+} from 'class-validator';
 
 export class CreateSongDTO {
   @IsString()
   @IsNotEmpty()
   readonly title: string;
-  @IsArray()
   @IsNotEmpty()
-  @IsString()
+  @IsArray()
   readonly artist: string[];
   @IsNotEmpty()
   @IsDateString()
