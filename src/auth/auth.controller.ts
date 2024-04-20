0329e0e2e8eebffd9f1c04729e83e24604e82c58
bryanvengwa <bmvengwa@gmail.com>
2024-04-20 16:44:14 +0200

    create create-user-dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('auth')
-export class AuthController {}
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateUserDTO } from "src/users/dto/create-user.dto";
+import { User } from "src/users/user.entity";
+import { UsersService } from "src/users/users.service";
+@Controller("auth")
+export class AuthController {
+  constructor(private userService: UsersService) {}
+  @Post("signup")
+  signup(
+@Body()
+    userDTO: CreateUserDTO
+  ): Promise<User> {
+    return this.userService.create(userDTO);
+  }
+}
