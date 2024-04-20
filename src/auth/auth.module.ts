c00b7d11b69d959290bada12c3882e54b218ee33
bryanvengwa <bmvengwa@gmail.com>
2024-04-20 19:38:38 +0200

    created constants files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports:[UsersModule],
+  imports: [UsersModule, JwtModule.register({ secret: 'fdafasf' })],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,19 +3,26 @@ import { LoginDTO } from "./dto/login.dto";
 import { UsersService } from "src/users/users.service";
 import * as bcrypt from "bcryptjs";
 import { User } from "src/users/users.entity";
+import { JwtService } from "@nestjs/jwt";
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(private userService: UsersService , private jwtService : JwtService) {}
 
-async login(loginDTO: LoginDTO): Promise<User>{
+async login(loginDTO: LoginDTO): Promise<{ accessToken: string }>{
 const user = await this.userService.findOne(loginDTO); // 1.
  const passwordMatched = await bcrypt.compare(
       loginDTO.password,
 user.password
 );
 if (passwordMatched) {
-delete user.password; // 4. 
-return user;
+    delete user.password;
+ 
+    // Sends JWT Token back in the response
+    const payload = { email: user.email, sub: user.id };
+    return {
+        accessToken: this.jwtService.sign(payload),
+    };
+    
 } else {
 throw new UnauthorizedException("Password does not match"); // 5.
 } 
