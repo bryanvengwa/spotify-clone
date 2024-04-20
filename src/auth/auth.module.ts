cbc0bf7abc8be703357e9384e0f91b5c1a13f6f5
bryanvengwa <bmvengwa@gmail.com>
2024-04-20 19:41:15 +0200

    created sign in options for the jwt tokens

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,9 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { authConstants } from './auth.constants';
 @Module({
-  imports: [UsersModule, JwtModule.register({ secret: 'fdafasf' })],
+  imports: [UsersModule, JwtModule.register({ secret: authConstants.secret, signOptions : {expiresIn : '1d'} })],
   providers: [AuthService],
   controllers: [AuthController],
 })
