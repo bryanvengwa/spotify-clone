f13addf4e9e1416983c863f0f4a9abffc22129d4
bryanvengwa <bmvengwa@gmail.com>
2024-04-20 20:54:08 +0200

    created a get profile method controller in the app module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get(  )
+  getProfile(@Req() request ,): any{
+    return request.user
+
   }
 }
