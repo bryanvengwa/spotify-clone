013578bcf06b5399c4d32ca33e7c1c0d138ee64c
bryanvengwa <bmvengwa@gmail.com>
2024-04-20 15:24:40 +0200

    created plalist-create-dto

---
+++ src/playlist/playlist.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Playlist } from './playlist.entity';
+import { Repository } from 'typeorm';
+import { CreatePlayListDto } from './dto/create-playlist.dto';
+import { Song } from 'src/songs/song.entity';
+import { User } from 'src/users/users.entity';
+
+@Injectable()
+export class PlayListsService {
+  constructor(
+    @InjectRepository(Playlist)
+    private playListRepo: Repository<Playlist>,
+    @InjectRepository(Song)
+    private songsRepo: Repository<Song>,
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+  ) {}
+  async create(playListDTO: CreatePlayListDto): Promise<Playlist> {
+    const playList = new Playlist();
+    playList.name = playListDTO.name;
+    // songs will be the array of IDs that we are getting from the DTO object
+    const songs = await this.songsRepo.findByIds(playListDTO.songs); //Set the relation for the songs with the playlist entity playList.songs = songs;
+    // A user will be the ID of the user we are getting from the request
+    //When we implemented the user authentication this id will become the logged
+    // in user id
+    const user = await this.userRepo.findOneBy({ id: playListDTO.user });
+    playList.user = user;
+    return this.playListRepo.save(playList);
+  }
+}
