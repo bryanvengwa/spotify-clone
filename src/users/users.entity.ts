50eb3d3eb06b343f904febdfdda575d582fabb13
bryanvengwa <bmvengwa@gmail.com>
2024-04-20 16:57:36 +0200

    created the missing users module in the auth module

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import { Playlist } from 'src/playlist/playlist.entity';
 import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
 
@@ -10,10 +11,11 @@ export class User {
   @Column()
   lastName: string;
 
-  @Column()
+  @Column({unique:true})
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
   @OneToMany(()=> Playlist , (playlist)=> playlist.user)
   playLists: Playlist [];
