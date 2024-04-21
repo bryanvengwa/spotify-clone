dbbd0613f69454ea3269417410063a4e3df0ac51
bryanvengwa <bmvengwa@gmail.com>
2024-04-21 10:45:08 +0200

    created artist service

--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
 
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Artist } from "./artist.entity";
 @Injectable()
-export class ArtistsService {}
+export class ArtistsService {
+  constructor(
+    @InjectRepository(Artist)
+    private artistRepo: Repository<Artist>
+) {}
+  findArtist(userId: number): Promise<Artist> {
+    return this.artistRepo.findOneBy({ user: { id: userId } });
+} }
