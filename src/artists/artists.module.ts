7ed790a74daf65b3bc1ca3305f39ecc4136ffb80
bryanvengwa <bmvengwa@gmail.com>
2024-04-21 10:38:08 +0200

    created an artist service file

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ArtistsService } from './artists.service';
 
-@Module({})
+@Module({
+  providers: [ArtistsService]
+})
 export class ArtistsModule {}
