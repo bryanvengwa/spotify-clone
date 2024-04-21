c23e4a552961862d704a0f8a188057aafd520467
bryanvengwa <bmvengwa@gmail.com>
2024-04-21 10:41:38 +0200

    created artists controller

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Artist } from './artist.entity';
+import { ArtistsController } from './artists.controller';
 
 @Module({
   imports : [TypeOrmModule.forFeature([Artist])],
-  providers: [ArtistsService]
+  providers: [ArtistsService],
+  controllers: [ArtistsController]
 })
 export class ArtistsModule {}
