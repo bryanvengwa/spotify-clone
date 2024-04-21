4ed78cb4df8312300d1981046f2e2d505c52d87d
bryanvengwa <bmvengwa@gmail.com>
2024-04-21 10:40:44 +0200

    imported artist entity into the module

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Artist } from './artist.entity';
 
 @Module({
+  imports : [TypeOrmModule.forFeature([Artist])],
   providers: [ArtistsService]
 })
 export class ArtistsModule {}
