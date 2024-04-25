ea5b7c8b25465f44c5452076f82e5db6be56596c
bryanvengwa <bmvengwa@gmail.com>
2024-04-25 06:17:28 +0200

    created disable-2fa endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDTO } from 'src/users/dto/create-user.dto';
 import { User } from 'src/users/users.entity';
 import { UsersService } from 'src/users/users.service';
@@ -33,23 +40,29 @@ export class AuthController {
   @Post('enable-2fa')
   @UseGuards(JwtAuthGuard)
   enable2FA(
-    @Request() 
+    @Request()
     req,
   ): Promise<Enable2FAType> {
     console.log(req.user);
     return this.authService.enable2FA(req.user.userId);
   }
   @Post('validate-2fa')
-@UseGuards(JwtAuthGuard)
-validate2FA(
-@Request()
-req,
-@Body()
-ValidateTokenDTO: ValidateTokenDTO,
-): Promise<{ verified: boolean }> {
-return this.authService.validate2FAToken(
-req.user.userId,
-ValidateTokenDTO.token,
-);
-}
+  @UseGuards(JwtAuthGuard)
+  validate2FA(
+    @Request()
+    req,
+    @Body()
+    ValidateTokenDTO: ValidateTokenDTO,
+  ): Promise<{ verified: boolean }> {
+    return this.authService.validate2FAToken(
+      req.user.userId,
+      ValidateTokenDTO.token,
+    );
+  }
+
+  @Get('disable-2fa')
+  @UseGuards(JwtAuthGuard)
+  disable2FA(@Request() req) {
+    return this.authService.disable2FA(req.user.userId);
+  }
 }
