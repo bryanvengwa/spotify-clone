88c4551ca2a829da8efdd8c0ecc50b6be627cfc4
bryanvengwa <bmvengwa@gmail.com>
2024-04-28 18:35:05 +0200

    utilised the config service

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SeedService } from './seeds/seeds.service';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
   // const seedService = app.get(SeedService)
   // await  seedService.seed();
-  await app.listen(parseInt(process.env.PORT));
+  const configService = app.get(ConfigService)
+
+  await app.listen(configService.get<number>('port'));
 }
 bootstrap();
