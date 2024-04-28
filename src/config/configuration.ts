cdde811ab10ea5841332250140ca47ded04a6703
bryanvengwa <bmvengwa@gmail.com>
2024-04-28 18:57:35 +0200

    created database configurations

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,4 +1,10 @@
 export default () => ({
   port: parseInt(process.env.PORT),
   secret: process.env.SECRET_KEY,
+
+//   DATABASE CONFIGURATIONS
+  dbHost: process.env.DB_HOST,
+  dbPort: parseInt(process.env.DB_PORT),
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
 });
